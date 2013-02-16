632ca016b1cc2db4793949de5b79eeeec82193e8
Syohei YOSHIDA <syohex@gmail.com>
2013-02-16 16:17:10 +0900

    Translate comment into English(#1)

--- init-loader.el
+++ init-loader.el
@@ -21,22 +21,33 @@
 
 ;;; Commentary:
 
-;;; 使い方
-;; load-pathの通った場所に置いて
+;;
+;; Load `init-loader.el'
+;;
 ;; (require 'init-loader)
+;;
+;; Load configuration files in `/path/to/init-directory'.
+;; If you omit arguments, `init-loader.el' loads from `init-loader-directory'
+;;
 ;; (init-loader-load "/path/to/init-directory")
-
-;;  デフォルト設定の場合,以下の順序で引数に渡したディレクトリ以下のファイルをロードする.
-;; 引数が省略された場合は,変数`init-loader-directory'の値を使用する.デフォルトは"~/.emacs.d/inits".
-
-;; 1. ソートされた,二桁の数字から始まるファイル. e.x, "00_utils.el" "01_ik-cmd.el" "21_javascript.el" "99_global-keys.el"
-;; 2. meadowの場合, meadow から始まる名前のファイル. e.x, "meadow-cmd.el" "meadow-config.el"
-;; 3. carbon-emacsの場合, carbon-emacs から始まる名前のファイル. e.x, "carbon-emacs-config.el" "carbon-emacs-migemo.el"
-;; 4. windowシステム以外の場合(terminal), nw から始まる名前のファイル e.x, "nw-config.el"
-
-;; ファイルロード後,変数`init-loader-show-log-after-init'の値がnon-nilなら,ログバッファを表示する関数を`after-init-hook'へ追加する.
-;; ログの表示は, M-x init-loader-show-log でも可能.
-
+;;
+;;
+;; Configuration files are loaded by following rules.
+;;   1. Configuration files which start with two digits.
+;;      Small number file is loaded earlier than large number file.
+;;      e.g. "00_utils.el" "01_ik-cmd.el" "21_javascript.el" ... "99_global-keys.el"
+;;
+;;   2. Windows specific configuration files if system is Windows.
+;;      First load files that start with 'windows-'. Second load files that start with 'meadow-'
+;;      e.g. "windows-fonts.el", "windows-system.el", "meadow-commands.el", "meadow-fonts.el"
+;;
+;;   3. MacOSX specific configuration files if system is MacOSX
+;;      First load files that start with 'carbon-'. Second load files that start with 'cocoa-'
+;;      e.g. "carbon-applescript.el", "cocoa-fonts.el", "cocoa-plist.el"
+;;
+;;   4. No window Emacs specific configuration files which start with 'nw-'
+;;      e.g. "nw-config.el", "nw-key.el"
+;;
 ;;; Code:
 
 (eval-when-compile (require 'cl))
