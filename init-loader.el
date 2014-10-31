128ee76adbf431f0b8c30a3a29cb20c9c5100cde
Syohei YOSHIDA <syohex@gmail.com>
2014-10-31 15:33:48 +0900

    Bump up version and update Changes

--- init-loader.el
+++ init-loader.el
@@ -2,7 +2,7 @@
 
 ;; Author: IMAKADO <ken.imakado@gmail.com>
 ;; URL: https://github.com/emacs-jp/init-loader/
-;; Version: 0.01
+;; Version: 0.02
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
